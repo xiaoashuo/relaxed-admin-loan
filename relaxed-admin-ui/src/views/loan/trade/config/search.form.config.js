23510cb15957ca97f0f2107c6d56e76a0b23dcfc
Yakir <1412844668@qq.com>
2023-03-08 15:01:10 +0800

    :feat  改变表结构

--- relaxed-admin-ui/src/views/loan/trade/config/search.form.config.js
+++ relaxed-admin-ui/src/views/loan/trade/config/search.form.config.js
@@ -1,6 +1,26 @@
 export const searchFormConfig={
   showHeader:true,
   formItems:[
-                                                                                                                                             
+
+    {
+      type:'text',
+      label: '合作方编号',
+      field:'partnerBizNo',
+      placeholder:'请输入合作方编号',
+      config:{
+        clearable: true
+      }
+
+    },
+    {
+      type:'text',
+      label: '交易流水号',
+      field:'tradeNo',
+      placeholder:'请输入交易流水号',
+      config:{
+        clearable: true
+      }
+
+    },
   ],
 }
