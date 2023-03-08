4877e705038207213643976a0babf02ad9183b3c
Yakir <1412844668@qq.com>
2023-03-08 15:54:16 +0800

    :feat  筛选条件增加

--- relaxed-admin-ui/src/views/loan/project/config/protmp.search.form.config.js
+++ relaxed-admin-ui/src/views/loan/project/config/protmp.search.form.config.js
@@ -1,6 +1,12 @@
 export const searchFormConfig={
   showHeader:true,
   formItems:[
-                                                                     
+    {
+      type:'text',
+      label: '项目模板id',
+      field:'projectTemplateId',
+      placeholder:'请输入项目模板id',
+
+    },
   ],
 }
--- relaxed-admin-ui/src/views/loan/project/config/search.config.js
+++ relaxed-admin-ui/src/views/loan/project/config/search.config.js
@@ -1,6 +1,12 @@
 export const searchFormConfig={
   showHeader:true,
   formItems:[
+    {
+      type:'text',
+      label: '项目id',
+      field:'projectId',
+      placeholder:'请输入项目id',
 
+    },
   ],
 }
